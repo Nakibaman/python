a960b876f9f836ba7c56a23cbf30409dbe3df3f3
Nakibaman <nakibaman@gmail.com>
2019-03-15 08:30:22 +0600

    08.03.19
    08.03.19

---
+++ 08.03.2019/2nd_problem.py
@@ -0,0 +1,5 @@
+fn=raw_input("Enter your first name :")
+
+ln=raw_input("Enter your last name :")
+
+print "Your full name is",fn,ln
---
+++ 08.03.2019/firstpython.py
@@ -0,0 +1,3 @@
+print "Enter your name:"
+name=raw_input()
+print "Good evening ",name
